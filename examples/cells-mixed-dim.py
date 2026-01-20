abf93b74ba2ea91131f5d4ef9e81d2824f5a6523
Tim Monko <timmonko@gmail.com>
2026-01-20 10:01:32 -0600

    remove examples/

--- examples/cells-mixed-dim.py
+++
@@ -1,8 +0,0 @@
-import napari
-
-viewer = napari.Viewer()
-
-viewer.open_sample(plugin='napari-metadata', sample='nuclei-md')
-viewer.window.add_plugin_dock_widget(plugin_name='napari-metadata')
-
-napari.run()
--- examples/hiPSCs-3d-mip.py
+++
@@ -1,8 +0,0 @@
-import napari
-
-viewer = napari.Viewer()
-
-viewer.open_sample(plugin='napari-metadata', sample='hipsc-3d-mip')
-viewer.window.add_plugin_dock_widget(plugin_name='napari-metadata')
-
-napari.run()
