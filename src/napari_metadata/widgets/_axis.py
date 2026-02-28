napari/napari-metadata

commit dbcf350f50c4a0aad1fabf68e90d4c2b1b6b07be

Fix widget flashing on init by properly parenting (#95)
# References and relevant issues

Depends on #94

Here is the diff
https://github.com/TimMonko/napari-metadata/compare/mnt/proj-reorg...TimMonko:napari-metadata:fix/widget-flash

# Description

This passes the parent to the collapsible widgets so that they do not
briefly flash before being docked in the main container. Tested locally
with napari.

src/napari_metadata/widgets/_main.py
@@ -103,12 +103,12 @@ class MetadataWidget(QWidget):
         )
         self._connect_file_general_metadata_components()
 
-        self._vert_file_general_metadata_container: QWidget = QWidget()
+        self._vert_file_general_metadata_container: QWidget = QWidget(self)
         self._vert_file_general_metadata_layout: QGridLayout = QGridLayout()
         self._vert_file_general_metadata_container.setLayout(
             self._vert_file_general_metadata_layout
         )
-        self._hori_file_general_metadata_container: QWidget = QWidget()
+        self._hori_file_general_metadata_container: QWidget = QWidget(self)
         self._hori_file_general_metadata_layout: QGridLayout = QGridLayout()
         self._hori_file_general_metadata_container.setLayout(
             self._hori_file_general_metadata_layout
@@ -116,12 +116,12 @@ class MetadataWidget(QWidget):
 
         self._axis_metadata_instance = AxisMetadata(napari_viewer, self)
 
-        self._vert_axis_metadata_container: QWidget = QWidget()
+        self._vert_axis_metadata_container: QWidget = QWidget(self)
         self._vert_axis_metadata_layout: QGridLayout = QGridLayout()
         self._vert_axis_metadata_container.setLayout(
             self._vert_axis_metadata_layout
         )
-        self._hori_axis_metadata_container: QWidget = QWidget()
+        self._hori_axis_metadata_container: QWidget = QWidget(self)
         self._hori_axis_metadata_layout: QGridLayout = QGridLayout()
         self._hori_axis_metadata_container.setLayout(
             self._hori_axis_metadata_layout
@@ -132,25 +132,25 @@ class MetadataWidget(QWidget):
             napari_viewer, self
         )
 
-        self._vert_inheritance_container: QWidget = QWidget()
+        self._vert_inheritance_container: QWidget = QWidget(self)
         self._vert_inhertiance_layout: QGridLayout = QGridLayout()
         self._vert_inheritance_container.setLayout(
             self._vert_inhertiance_layout
         )
-        self._hori_inheritance_container: QWidget = QWidget()
+        self._hori_inheritance_container: QWidget = QWidget(self)
         self._hori_inheritance_layout: QGridLayout = QGridLayout()
         self._hori_inheritance_container.setLayout(
             self._hori_inheritance_layout
         )
 
-        vertical_container: QScrollArea = QScrollArea()
+        vertical_container: QScrollArea = QScrollArea(self)
         vertical_container.setWidgetResizable(True)
         vertical_container.setHorizontalScrollBarPolicy(
             Qt.ScrollBarPolicy.ScrollBarAlwaysOff
         )
         vertical_container.container_orientation = 'vertical'
 
-        vertical_content = QWidget()
+        vertical_content = QWidget(vertical_container)
         vertical_content.setLayout(self._vertical_layout)
         vertical_container.setWidget(vertical_content)
 
@@ -208,7 +208,7 @@ class MetadataWidget(QWidget):
         self._vertical_layout.addStretch(1)
 
         horizontal_container: HorizontalOnlyOuterScrollArea = (
-            HorizontalOnlyOuterScrollArea()
+            HorizontalOnlyOuterScrollArea(self)
         )
         horizontal_container.setWidgetResizable(True)
         horizontal_container.setVerticalScrollBarPolicy(
@@ -219,7 +219,7 @@ class MetadataWidget(QWidget):
         )
         horizontal_container.container_orientation = 'horizontal'
 
-        horizontal_content: QWidget = QWidget()
+        horizontal_content: QWidget = QWidget(horizontal_container)
         horizontal_content.setLayout(self._horizontal_layout)
         horizontal_container.setWidget(horizontal_content)
         self._stacked_layout.addWidget(horizontal_container)
@@ -274,7 +274,7 @@ class MetadataWidget(QWidget):
         )
         self._horizontal_layout.addStretch(1)
 
-        no_layer_container: QWidget = QWidget()
+        no_layer_container: QWidget = QWidget(self)
         no_layer_container.container_orientation = 'no_layer'
         no_layer_container.setLayout(self._no_layer_layout)
         self._no_layer_label: QLabel = QLabel(

src/napari_metadata/widgets/_containers.py
@@ -44,7 +44,7 @@ class CollapsibleSectionContainer(QWidget):
         main_widget: MetadataWidgetAPI,
         orientation: Literal['vertical', 'horizontal'] = 'vertical',
     ):
-        super().__init__()
+        super().__init__(parent=main_widget)
         self._viewer = viewer
         self._container_name = container_name
         self._main_widget = main_widget
@@ -72,7 +72,7 @@ class CollapsibleSectionContainer(QWidget):
         self._layout.addWidget(self._button, 0)
 
         # Create expanding area
-        self._expanding_area = QScrollArea()
+        self._expanding_area = QScrollArea(self)
         self._expanding_area.setWidgetResizable(True)
 
         if orientation == 'vertical':
@@ -191,7 +191,7 @@ class CollapsibleSectionContainer(QWidget):
             self._expanding_area.setWidget(setting_widget)
         else:  # horizontal
             # Horizontal containers need a wrapper with stretch
-            wrapper = QWidget()
+            wrapper = QWidget(self._expanding_area)
             wrapper_layout = QVBoxLayout(wrapper)
             wrapper_layout.setContentsMargins(0, 0, 0, 0)
             wrapper_layout.addWidget(setting_widget)

commit 06f4e2b84b1c1ded7f1c01de38df0529a44a3540

properly inherit parents (was regression after #95)

src/napari_metadata/widgets/_axis.py
@@ -69,11 +69,11 @@ class AxisLabels(AxisComponentBase):
     def __init__(
         self,
         viewer: ViewerModel,
-        main_widget: QWidget,
+        parent_widget: QWidget,
         *,
         on_labels_changed: Callable[[], None] | None = None,
     ) -> None:
-        super().__init__(viewer, main_widget)
+        super().__init__(viewer, parent_widget)
         self._on_labels_changed = on_labels_changed
         self._line_edits: list[QLineEdit] = []
 
@@ -87,11 +87,11 @@ class AxisLabels(AxisComponentBase):
             return
         for i in range(ndim):
             # Index label (not the axis name)
-            index_label = QLabel(str(i))
+            index_label = QLabel(str(i), parent=self._parent_widget)
             index_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
             self._axis_name_labels.append(index_label)
 
-            line_edit = QLineEdit()
+            line_edit = QLineEdit(parent=self._parent_widget)
             line_edit.setText(labels[i] if i < len(labels) else '')
             line_edit.editingFinished.connect(self._on_editing_finished)
             self._line_edits.append(line_edit)
@@ -135,8 +135,8 @@ class AxisTranslations(AxisComponentBase):
 
     _label_text = 'Translate:'
 
-    def __init__(self, viewer: ViewerModel, main_widget: QWidget) -> None:
-        super().__init__(viewer, main_widget)
+    def __init__(self, viewer: ViewerModel, parent_widget: QWidget) -> None:
+        super().__init__(viewer, parent_widget)
         self._spinboxes: list[QDoubleSpinBox] = []
 
     def _all_widget_lists(self) -> list[list[QWidget]]:
@@ -149,7 +149,7 @@ class AxisTranslations(AxisComponentBase):
         if ndim == 0:
             return
         for value in translations:
-            sb = QDoubleSpinBox()
+            sb = QDoubleSpinBox(parent=self._parent_widget)
             sb.setDecimals(1)
             sb.setSingleStep(1.0)
             sb.setRange(-1_000_000, 1_000_000)
@@ -193,8 +193,8 @@ class AxisScales(AxisComponentBase):
 
     _label_text = 'Scale:'
 
-    def __init__(self, viewer: ViewerModel, main_widget: QWidget) -> None:
-        super().__init__(viewer, main_widget)
+    def __init__(self, viewer: ViewerModel, parent_widget: QWidget) -> None:
+        super().__init__(viewer, parent_widget)
         self._spinboxes: list[QDoubleSpinBox] = []
 
     # -- AxisComponentBase overrides ----------------------------------------
@@ -209,7 +209,7 @@ class AxisScales(AxisComponentBase):
         if ndim == 0:
             return
         for value in scales:
-            sb = QDoubleSpinBox()
+            sb = QDoubleSpinBox(parent=self._parent_widget)
             sb.setDecimals(3)
             sb.setSingleStep(0.1)
             sb.setRange(0.001, 1_000_000)
@@ -262,8 +262,8 @@ class AxisUnits(AxisComponentBase):
 
     _label_text = 'Units:'
 
-    def __init__(self, viewer: ViewerModel, main_widget: QWidget) -> None:
-        super().__init__(viewer, main_widget)
+    def __init__(self, viewer: ViewerModel, parent_widget: QWidget) -> None:
+        super().__init__(viewer, parent_widget)
         self._type_comboboxes: list[QComboBox] = []
         self._unit_comboboxes: list[QComboBox] = []
         self._unit_line_edits: list[QLineEdit] = []
@@ -287,12 +287,12 @@ class AxisUnits(AxisComponentBase):
             unit_str = str(layer_units[i]) if i < len(layer_units) else ''
 
             # Type combobox (space / time / string)
-            type_cb = QComboBox()
+            type_cb = QComboBox(parent=self._parent_widget)
             for axis_type in AxisUnitEnum:
                 type_cb.addItem(str(axis_type), axis_type)
 
             # Unit combobox (curated pint units)
-            unit_cb = QComboBox()
+            unit_cb = QComboBox(parent=self._parent_widget)
             matched_type = self._populate_unit_combobox(unit_str, unit_cb)
             type_index = type_cb.findData(
                 matched_type
@@ -302,7 +302,7 @@ class AxisUnits(AxisComponentBase):
             type_cb.setCurrentIndex(type_index)
 
             # Free-form line edit for STRING type
-            line_edit = QLineEdit()
+            line_edit = QLineEdit(parent=self._parent_widget)
 
             self._type_comboboxes.append(type_cb)
             self._unit_comboboxes.append(unit_cb)
@@ -500,15 +500,15 @@ class AxisMetadata:
     * Toggle inheritance checkboxes globally
     """
 
-    def __init__(self, viewer: ViewerModel, main_widget: QWidget) -> None:
+    def __init__(self, viewer: ViewerModel, parent_widget: QWidget) -> None:
         self._labels = AxisLabels(
             viewer,
-            main_widget,
+            parent_widget,
             on_labels_changed=self._on_labels_changed,
         )
-        self._translations = AxisTranslations(viewer, main_widget)
-        self._scales = AxisScales(viewer, main_widget)
-        self._units = AxisUnits(viewer, main_widget)
+        self._translations = AxisTranslations(viewer, parent_widget)
+        self._scales = AxisScales(viewer, parent_widget)
+        self._units = AxisUnits(viewer, parent_widget)
 
         self._components: list[AxisComponentBase] = [
             self._labels,

src/napari_metadata/widgets/_base.py
@@ -76,13 +76,13 @@ class AxisComponentBase(ABC):
     def __init__(
         self,
         viewer: ViewerModel,
-        main_widget: QWidget,
+        parent_widget: QWidget,
     ) -> None:
         self._napari_viewer: ViewerModel = viewer
-        self._main_widget = main_widget
+        self._parent_widget = parent_widget
         self._selected_layer: Layer | None = None
 
-        self._component_qlabel = QLabel(self._label_text)
+        self._component_qlabel = QLabel(self._label_text, parent=parent_widget)
         self._component_qlabel.setStyleSheet('font-weight: bold')
         self._component_qlabel.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
@@ -163,7 +163,7 @@ class AxisComponentBase(ABC):
         merged: list[Any] = [
             tv if self._inherit_checkboxes[i].isChecked() else cv
             for i, (cv, tv) in enumerate(
-                zip(current_values, template_values, strict=False)
+                zip(current_values, template_values, strict=True)
             )
         ]
         self._apply_values(merged)
@@ -229,14 +229,17 @@ class AxisComponentBase(ABC):
         """
         labels = get_axes_labels(self._napari_viewer, layer)
         for i, label in enumerate(labels):
-            qlabel = QLabel(label if label else str(i))
+            qlabel = QLabel(
+                label if label else str(i),
+                parent=self._parent_widget,
+            )
             qlabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
             self._axis_name_labels.append(qlabel)
 
     def _create_inherit_checkboxes(self, layer: Layer) -> None:
         """Create one inherit ``QCheckBox`` per axis (all checked)."""
         for _ in range(get_layer_dimensions(layer)):
-            cb = QCheckBox('')
+            cb = QCheckBox('', parent=self._parent_widget)
             cb.setChecked(True)
             cb.setFocusPolicy(Qt.FocusPolicy.NoFocus)
             self._inherit_checkboxes.append(cb)

src/napari_metadata/widgets/_main.py
@@ -13,7 +13,6 @@ from qtpy.QtWidgets import (
     QLabel,
     QLayout,
     QLayoutItem,
-    QLineEdit,
     QMainWindow,
     QScrollArea,
     QSizePolicy,
@@ -87,7 +86,6 @@ class MetadataWidget(QWidget):
         self._general_metadata_instance = FileGeneralMetadata(
             napari_viewer, self
         )
-        self._connect_file_general_metadata_components()
 
         self._vert_file_general_metadata_container: QWidget = QWidget()
         self._vert_file_general_metadata_layout: QGridLayout = QGridLayout()
@@ -119,9 +117,9 @@ class MetadataWidget(QWidget):
         )
 
         self._vert_inheritance_container: QWidget = QWidget()
-        self._vert_inhertiance_layout: QGridLayout = QGridLayout()
+        self._vert_inheritance_layout: QGridLayout = QGridLayout()
         self._vert_inheritance_container.setLayout(
-            self._vert_inhertiance_layout
+            self._vert_inheritance_layout
         )
         self._hori_inheritance_container: QWidget = QWidget()
         self._hori_inheritance_layout: QGridLayout = QGridLayout()
@@ -784,7 +782,7 @@ class MetadataWidget(QWidget):
             hori_axis_layout.parentWidget().updateGeometry()
 
     def _set_inheritance_orientation(self, orientation: str) -> None:
-        vert_inheritance_layout: QGridLayout = self._vert_inhertiance_layout
+        vert_inheritance_layout: QGridLayout = self._vert_inheritance_layout
         vert_inheritance_layout.setVerticalSpacing(8)
         hori_inheritance_layout: QGridLayout = self._hori_inheritance_layout
 
@@ -798,34 +796,6 @@ class MetadataWidget(QWidget):
             setting_layout = hori_inheritance_layout
 
         setting_layout.addWidget(self._inheritance_instance)
-
-    def _connect_file_general_metadata_components(self) -> None:
-        file_general_meta_instance: FileGeneralMetadata = (
-            self._general_metadata_instance
-        )
-        components_dict = (
-            file_general_meta_instance._file_metadata_components_dict
-        )  # type: ignore
-
-        for name in components_dict:
-            general_component: MetadataComponent = components_dict[name]  # type: ignore
-            entries_dict: dict[str, tuple[QWidget, int, int, str]] = (
-                general_component.get_entries_dict(self._current_orientation)
-            )
-            for entry_name in entries_dict:
-                entry_widget: QWidget = entries_dict[entry_name][0]
-                method_name: str = entries_dict[entry_name][3]
-                if method_name == '':
-                    continue
-                if isinstance(entry_widget, QLineEdit):
-                    entry_line_edit: QLineEdit = cast(QLineEdit, entry_widget)
-                    entry_line_edit.textEdited.connect(
-                        getattr(self, method_name)
-                    )
-
-    def _on_name_line_changed(self, text: str) -> None:
-        """Connected by _connect_file_general_metadata_components (PR 4 cleanup)."""
-        return
 
     def _set_layout_type(self, layout_type: str) -> None:
         if layout_type == 'vertical':