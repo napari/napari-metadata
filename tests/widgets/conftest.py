c4add13c50f7ed0e6b8913b0684f77190c6a992a
Tim Monko <timmonko@gmail.com>
2026-02-28 18:12:25 -0600

    add note about widget.isHidden() in conftest

--- tests/widgets/conftest.py
+++ tests/widgets/conftest.py
@@ -14,4 +14,8 @@ def viewer_model() -> ViewerModel:
 def parent_widget(qtbot) -> QWidget:
     widget = QWidget()
     qtbot.addWidget(widget)
+    # don't use widget.show() in tests, because that causes each test to pop up a window
+    # as such, using widget.isVisible() in tests will return False, but that's fine for our purposes
+    # instead we CAN check for widget.isHidden() to verify that the widget is not visible in the headless state because
+    # .isVisible() requires even the parent to be visible, while .isHidden() only checks the widget itself
     return widget
