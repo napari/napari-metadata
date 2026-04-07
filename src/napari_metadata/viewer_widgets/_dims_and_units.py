024f9f56bfaa1e865f0a42a2090374e4fc639899
Carlos Rodriguez <carlosmariorr90@hotmail.com>
2026-04-07 22:31:04 +0900

    Made individual containers for the viewer, layer and setting axis labels.

--- src/napari_metadata/viewer_widgets/_dims_and_units.py
+++ src/napari_metadata/viewer_widgets/_dims_and_units.py
@@ -2,11 +2,13 @@
 
 from __future__ import annotations
 
+from collections.abc import Sequence
 from typing import TYPE_CHECKING
 
+from napari.layers import Layer
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import (
-    QGridLayout,
+    QHBoxLayout,
     QLabel,
     QLayout,
     QPushButton,
@@ -23,7 +25,7 @@ class DimsAndUnitsWidget(QWidget):
 
     def __init__(
         self,
-        napari_viewer,
+        napari_viewer: ViewerModel,
         *,
         parent: QWidget | None = None,
     ):
@@ -61,7 +63,7 @@ class AxisLabelsDisplayWidget(QWidget):
         self._layout.addWidget(self._title_label)
 
         self._labels_container: QWidget = QWidget(parent=self)
-        self._labels_layout: QGridLayout = QGridLayout(self._labels_container)
+        self._labels_layout: QHBoxLayout = QHBoxLayout(self._labels_container)
         self._labels_container.setLayout(self._labels_layout)
         self._layout.addWidget(self._labels_container)
 
@@ -84,53 +86,68 @@ class AxisLabelsDisplayWidget(QWidget):
         return
 
     def _populate_labels_grid(self) -> None:
-        number_of_dims = self._napari_viewer.dims.ndim
-        layer = self._napari_viewer.layers.selection.active
-        layer_dims = 0
-        if layer is not None:
-            layer_dims = layer.ndim
-        dims_difference = number_of_dims - layer_dims
         clear_layout(self._labels_layout)
-        index_label: QLabel = QLabel('Index', parent=self._labels_container)
-        index_label.setStyleSheet('font-weight: bold; font-size: 10pt')
-        index_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        viewer_label: QLabel = QLabel('Viewer', parent=self._labels_container)
-        viewer_label.setStyleSheet('font-weight: bold; font-size: 10pt')
-        viewer_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layer_label: QLabel = QLabel('Layer', parent=self._labels_container)
-        layer_label.setStyleSheet('font-weight: bold; font-size: 10pt')
-        layer_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self._labels_layout.addWidget(index_label, 0, 0)
-        self._labels_layout.addWidget(viewer_label, 0, 1)
-        self._labels_layout.addWidget(layer_label, 0, 2)
-        for i in range(number_of_dims):
-            current_index_label = QLabel(
-                str(i - number_of_dims), parent=self._labels_container
-            )
-            current_index_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-            current_viewer_label = QLabel(
-                self._napari_viewer.dims.axis_labels[i],
-                parent=self._labels_container,
-            )
-            current_viewer_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-            current_layer_text: str = ''
-            if i < dims_difference:
-                current_layer_text = str(i - number_of_dims)
-            else:
-                if layer is not None:
-                    current_layer_text = layer.axis_labels[i - dims_difference]
-            current_layer_label = QLabel(
-                current_layer_text, parent=self._labels_container
-            )
-            current_layer_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-            self._labels_layout.addWidget(current_index_label, i + 1, 0)
-            self._labels_layout.addWidget(current_viewer_label, i + 1, 1)
-            self._labels_layout.addWidget(current_layer_label, i + 1, 2)
+
+        ndim = self._napari_viewer.dims.ndim
+        layer = self._napari_viewer.layers.selection.active
+
+        index_list = [str(a - ndim) for a in range(ndim)]
+        index_container = LabelContainer('Index', index_list, self)
+
+        viewer_container = LabelContainer(
+            'Viewer', self._napari_viewer.dims.axis_labels
+        )
+
+        layer_labels = self._solve_layer_to_viewer_list(ndim, layer)
+        layer_container = LabelContainer('Layer', layer_labels)
+
+        setting_list = self._solve_setting_labels_list(
+            self._napari_viewer.dims.axis_labels, layer_labels
+        )
+        setting_container = LabelContainer('Set', setting_list)
+
+        self._labels_layout.addWidget(index_container)
+        self._labels_layout.addWidget(viewer_container)
+        self._labels_layout.addWidget(layer_container)
+        self._labels_layout.addWidget(setting_container)
 
     def _apply_layer_labels_to_viewer(self) -> None:
-        self._napari_viewer.dims.axis_labels = (
-            self._napari_viewer.layers.selection.active.axis_labels
+        if self._napari_viewer.layers.selection.active is None:
+            return
+        layer_labels = self._solve_layer_to_viewer_list(
+            self._napari_viewer.dims.ndim,
+            self._napari_viewer.layers.selection.active,
         )
+        self._napari_viewer.dims.axis_labels = tuple(
+            self._solve_setting_labels_list(
+                self._napari_viewer.dims.axis_labels, layer_labels
+            )
+        )
+
+    def _solve_layer_to_viewer_list(
+        self, ndim: int, layer: Layer | None
+    ) -> list[str]:
+        if layer is None:
+            return [''] * ndim
+        return_list: list[str] = []
+        dim_diff = ndim - layer.ndim
+        for i in range(ndim):
+            if i < dim_diff:
+                return_list.append('')
+            else:
+                return_list.append(layer.axis_labels[i - dim_diff])
+        return return_list
+
+    def _solve_setting_labels_list(
+        self, viewer_labels: Sequence[str], layer_labels: Sequence[str]
+    ) -> list[str]:
+        return_list: list[str] = []
+        for a in range(len(viewer_labels)):
+            if layer_labels[a] == '':
+                return_list.append(str(a - len(viewer_labels)))
+            else:
+                return_list.append(layer_labels[a])
+        return return_list
 
 
 def clear_layout(layout: QLayout) -> None:
@@ -145,3 +162,22 @@ def set_title_label_style(label: QLabel) -> QLabel:
     label.setStyleSheet('font-weight: bold;font-size: 15pt')
     label.setAlignment(Qt.AlignmentFlag.AlignCenter)
     return label
+
+
+class LabelContainer(QWidget):
+    def __init__(
+        self, title: str, labels: Sequence[str], parent: QWidget | None = None
+    ) -> None:
+        super().__init__(parent=parent)
+
+        self._layout = QVBoxLayout(self)
+        self.setLayout(self._layout)
+
+        self._title_label = QLabel(title, parent=self)
+        set_title_label_style(self._title_label)
+        self._layout.addWidget(self._title_label)
+
+        for label_text in labels:
+            label = QLabel(label_text, self)
+            label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            self._layout.addWidget(label)
